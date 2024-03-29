5837522ebd734a30f9ea9e51b54c3395d2bd59dd
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-29 14:32:19 +0200

    task-2 done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,66 @@
+const input = document.querySelector('input[name=delay]');
+const submitBtn = document.querySelector('button');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitHandle);
+input.addEventListener('input', inputHandle);
+
+function inputHandle(event) {
+  const inputData = event.target.value;
+  localStorage.setItem('delay', inputData);
+}
+
+function submitHandle(event) {
+  event.preventDefault();
+
+  const delay = localStorage.getItem('delay');
+  const choice = localStorage.setItem(
+    'state',
+    event.target.elements.state.value
+  );
+  const state = localStorage.getItem('state');
+
+  const iziSuccessOptions = {
+    title: '✅',
+    titleSize: '24px',
+    message: `Fulfilled promise in ${delay}ms`,
+    messageColor: 'white',
+    messageSize: '16px',
+    backgroundColor: 'lightgreen',
+    position: 'topRight',
+    timeout: 3000,
+  };
+
+  const iziRejectOptions = {
+    title: '❌',
+    titleSize: '24px',
+    message: `Rejected promise in ${delay}ms`,
+    messageColor: 'white',
+    messageSize: '16px',
+    backgroundColor: 'rgba(225, 0, 0, 0.3)',
+    position: 'topRight',
+    timeout: 3000,
+  };
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(iziToast.show(iziSuccessOptions));
+      } else {
+        reject(iziToast.show(iziRejectOptions));
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      console.log(value);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  form.reset();
+}
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
